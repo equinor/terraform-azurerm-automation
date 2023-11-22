7e6b981d6b91464edfb1a980055271fe4f82178b
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-11-22 11:46:40 +0100

    refactor: clarify use of data source

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,18 @@ output "account_id" {
 
 output "identity_principal_id" {
   description = "The principal ID of the system-assigned identity of this Automation account."
-  value       = try(data.azurerm_automation_account.this.identity[0].principal_id, null)
+
+  # Get "principal_id" from a data source to read its value during the the apply phase instead of the plan phase.
+  # This ensures the Automation account system-assigned identity is always enabled before trying to read its principal ID.
+  # Ref: https://developer.hashicorp.com/terraform/language/data-sources#data-resource-behavior
+  value = try(data.azurerm_automation_account.this.identity[0].principal_id, null)
 }
 
 output "identity_tenant_id" {
   description = "The tenant ID of the system-assigned identity of this Automation account."
-  value       = try(data.azurerm_automation_account.this.identity[0].tenant_id, null)
+
+  # Get "tenant_id" from a data source to read its value during the the apply phase instead of the plan phase.
+  # This ensures the Automation account system-assigned identity is always enabled before trying to read its tenant ID.
+  # Ref: https://developer.hashicorp.com/terraform/language/data-sources#data-resource-behavior
+  value = try(data.azurerm_automation_account.this.identity[0].tenant_id, null)
 }
