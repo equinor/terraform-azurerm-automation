56db9d2d330ec8ebef14d9b7d972584b54e61274
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-08-26 13:35:45 +0200

    chore: update variable and output descriptions

--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,11 @@ output "account_id" {
 }
 
 output "identity_principal_id" {
-  description = "The principal ID of the system-assigned identity of this Automation account."
+  description = "The principal ID of the system-assigned identity of this Automation account. This value will be null if the system-assigned identity is disabled."
   value       = try(azurerm_automation_account.this.identity[0].principal_id, null)
 }
 
 output "identity_tenant_id" {
-  description = "The tenant ID of the system-assigned identity of this Automation account."
+  description = "The tenant ID of the system-assigned identity of this Automation account. This value will be null if the system-assigned identity is disabled."
   value       = try(azurerm_automation_account.this.identity[0].tenant_id, null)
 }
